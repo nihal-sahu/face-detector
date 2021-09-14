e617155cf6ab422f4a14a1727036426cfadfd6b2
nihal-sahu <nihalsahu360@gmail.com>
2021-09-14 00:36:35 -0400

    Updated Files

--- capture.py
+++ capture.py
@@ -4,9 +4,8 @@ video = cv2.VideoCapture(0)
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-a = 1
 while True:
-    a += 1
+
     check, frame = video.read()
 
     print(check)
@@ -26,6 +25,5 @@ while True:
     if key == ord('q'):
         break
 
-print(a)
 video.release()
 cv2.destroyAllWindows()
